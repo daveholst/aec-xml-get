63de6bcdaab31cfd52d4b29442c563535658ae8f
Dave Holst <daveholst@gmail.com>
2022-04-30 08:50:27 +0800

    refactor and added json route

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,18 +1,14 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
-import { listData } from './routes/latest'
+import { latestHandler } from './routes/latest'
+import { latestJsonHandler } from './routes/latest-json'
 
 export const app: Express = express()
 
 app.get('/latest', async (req: Request, res: Response) => {
-  try {
-    const data = await listData()
-    res.type('application/xml').status(200).send(data)
-  } catch (error) {
-    res.send(error)
-  }
+  latestHandler(req, res)
 })
 
-// app.listen(port, () => {
-//   console.log(`⚡️[server]: Server is running  at https://localhost:${port}`)
-// })
+app.get('/latest-json', async (req: Request, res: Response) => {
+  latestJsonHandler(req, res)
+})
--- src/app/routes/latest.ts
+++ src/app/routes/latest.ts
@@ -1,55 +1,11 @@
-import * as ftp from 'basic-ftp'
-import StreamZip from 'node-stream-zip'
-import fs from 'fs/promises'
-import path from 'path'
+import { Request, Response } from 'express'
+import { getXmlData } from './shared/shared'
 
-export async function listData() {
-  const client = new ftp.Client()
-
-  const host = 'mediafeedarchive.aec.gov.au'
-  const electionCode = '24310'
-  const zipDir = `/${electionCode}/Standard/Verbose/`
-  const fileNamePrefix = 'aec-mediafeed-Standard-Verbose'
-
-  // client.ftp.verbose = true
+export async function latestHandler(req: Request, res: Response) {
   try {
-    await client.access({
-      host,
-    })
-    const listedData = await client.list(zipDir)
-
-    // build an array list of timestamps available
-    const timestamps = listedData.map((e) =>
-      parseInt(e.name.split('-').pop().slice(0, -3))
-    )
-    // find largest/latest number/timestamp
-    const newestTimestamp = Math.max(...timestamps)
-
-    // download the newest files
-    console.log(
-      `${zipDir}${fileNamePrefix}-${electionCode}-${newestTimestamp}.zip`
-    )
-    // download zip file
-    await client.downloadTo(
-      'src/results/results.zip',
-      `${zipDir}${fileNamePrefix}-${electionCode}-${newestTimestamp}.zip`
-    )
-    // close the ftp connection
-    client.close()
-    // extract the xml file
-    const zip = new StreamZip.async({ file: 'src/results/results.zip' })
-    await zip.extract(
-      `xml/aec-mediafeed-results-standard-verbose-${electionCode}.xml`,
-      './src/results/results.xml'
-    )
-    // close the zip connection
-    await zip.close()
-
-    // grab the xml file
-    const xmlLocation = path.join(__dirname, '../src/results/results.xml')
-    const xml = await fs.readFile(xmlLocation, 'utf8')
-    return xml
-  } catch (err) {
-    console.log(err)
+    const data = await getXmlData()
+    res.type('application/xml').status(200).send(data)
+  } catch (error) {
+    res.send(error)
   }
 }
